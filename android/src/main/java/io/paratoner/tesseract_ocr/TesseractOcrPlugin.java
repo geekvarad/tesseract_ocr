b85b85572584f21ae1710956a4968dd1cb044a17
varad <varads@iitk.ac.in>
2020-10-21 01:11:16 +0530

    v2

--- android/src/main/java/io/paratoner/tesseract_ocr/TesseractOcrPlugin.java
+++ android/src/main/java/io/paratoner/tesseract_ocr/TesseractOcrPlugin.java
@@ -2,55 +2,98 @@ package io.paratoner.tesseract_ocr;
 
 import com.googlecode.tesseract.android.TessBaseAPI;
 
+import androidx.annotation.NonNull;
+//import io.flutter.embedding.android.FlutterActivity;
+//import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import java.io.File;
-import io.flutter.plugin.common.MethodCall;
+//import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
-import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
-import io.flutter.plugin.common.MethodChannel.Result;
+//import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+//import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
+//import io.flutter.plugins.GeneratedPluginRegistrant;
 
 /** TesseractOcrPlugin */
-public class TesseractOcrPlugin implements MethodCallHandler {
+public class TesseractOcrPlugin implements FlutterPlugin {
 
   private static final int DEFAULT_PAGE_SEG_MODE = TessBaseAPI.PageSegMode.PSM_AUTO_OSD;
 
   /** Plugin registration. */
-  public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "tesseract_ocr");
-    channel.setMethodCallHandler(new TesseractOcrPlugin());
-  }
-
-
-  @Override
-  public void onMethodCall(MethodCall call, Result result) {
-
-    if (call.method.equals("extractText")) {
-      final String tessDataPath = call.argument("tessData");
-      final String imagePath = call.argument("imagePath");
-      String DEFAULT_LANGUAGE = "eng";
-      if(call.argument("language") != null){
-        DEFAULT_LANGUAGE = call.argument("language");
-      }
-      final String[] recognizedText = new String[1];
-      final TessBaseAPI baseApi = new TessBaseAPI();
-      baseApi.init(tessDataPath, DEFAULT_LANGUAGE);
-      final File tempFile = new File(imagePath);
-      baseApi.setPageSegMode(DEFAULT_PAGE_SEG_MODE);
-
-      Thread t = new Thread(new Runnable() {
-        public void run() {
-          baseApi.setImage(tempFile);
-          recognizedText[0] = baseApi.getUTF8Text();
-          baseApi.end();
-        }
-      });
-      t.start();
-      try { t.join(); } catch (InterruptedException e) { e.printStackTrace(); }
-      result.success(recognizedText[0]);
-    } else {
-      result.notImplemented();
+  // public static void registerWith(Registrar registrar) {
+  //   final MethodChannel channel = new MethodChannel(registrar.messenger(), "tesseract_ocr");
+  //   channel.setMethodCallHandler(new TesseractOcrPlugin());
+  // }
+    @Override
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+        
+        new MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "tesseract_ocr")
+                .setMethodCallHandler(
+                    (call, result) -> {
+                      if (call.method.equals("extractText")) {
+                        final String tessDataPath = call.argument("tessData");
+                        final String imagePath = call.argument("imagePath");
+                        String DEFAULT_LANGUAGE = "eng";
+                        if(call.argument("language") != null){
+                          DEFAULT_LANGUAGE = call.argument("language");
+                        }
+                        final String[] recognizedText = new String[1];
+                        final TessBaseAPI baseApi = new TessBaseAPI();
+                        baseApi.init(tessDataPath, DEFAULT_LANGUAGE);
+                        final File tempFile = new File(imagePath);
+                        baseApi.setPageSegMode(DEFAULT_PAGE_SEG_MODE);
+                  
+                        Thread t = new Thread(new Runnable() {
+                          public void run() {
+                            baseApi.setImage(tempFile);
+                            recognizedText[0] = baseApi.getUTF8Text();
+                            baseApi.end();
+                          }
+                        });
+                        t.start();
+                        try { t.join(); } catch (InterruptedException e) { e.printStackTrace(); }
+                        result.success(recognizedText[0]);
+                      } else {
+                        result.notImplemented();
+                      }}
+        );
     }
-  }
+    @Override
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+        // Do ntohing basically?
+    }
+
+
+  // @Override
+  // public void onMethodCall(MethodCall call, Result result) {
+
+  //   if (call.method.equals("extractText")) {
+  //     final String tessDataPath = call.argument("tessData");
+  //     final String imagePath = call.argument("imagePath");
+  //     String DEFAULT_LANGUAGE = "eng";
+  //     if(call.argument("language") != null){
+  //       DEFAULT_LANGUAGE = call.argument("language");
+  //     }
+  //     final String[] recognizedText = new String[1];
+  //     final TessBaseAPI baseApi = new TessBaseAPI();
+  //     baseApi.init(tessDataPath, DEFAULT_LANGUAGE);
+  //     final File tempFile = new File(imagePath);
+  //     baseApi.setPageSegMode(DEFAULT_PAGE_SEG_MODE);
+
+  //     Thread t = new Thread(new Runnable() {
+  //       public void run() {
+  //         baseApi.setImage(tempFile);
+  //         recognizedText[0] = baseApi.getUTF8Text();
+  //         baseApi.end();
+  //       }
+  //     });
+  //     t.start();
+  //     try { t.join(); } catch (InterruptedException e) { e.printStackTrace(); }
+  //     result.success(recognizedText[0]);
+  //   } else {
+  //     result.notImplemented();
+  //   }
+  // }
 
 
 }
--- example/android/app/src/main/java/io/paratoner/tesseract_ocr_example/MainActivity.java
+++ example/android/app/src/main/java/io/paratoner/tesseract_ocr_example/MainActivity.java
@@ -1,13 +1,26 @@
 package io.paratoner.tesseract_ocr_example;
 
-import android.os.Bundle;
-import io.flutter.app.FlutterActivity;
+//import android.os.Bundle;
+//import io.flutter.app.FlutterActivity;
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
+import io.flutter.embedding.engine.FlutterEngine;
 
 public class MainActivity extends FlutterActivity {
+  // @Override
+  // protected void onCreate(Bundle savedInstanceState) {
+  //   super.onCreate(savedInstanceState);
+  //   GeneratedPluginRegistrant.registerWith(this);
+  // }
   @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
+      public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+          GeneratedPluginRegistrant.registerWith(flutterEngine);
+          // new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL)
+          //         .setMethodCallHandler(
+          //             (call, result) -> {
+          //                 // Your existing code
+          //         }
+          // );
   }
 }
